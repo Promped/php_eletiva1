0b2e5da9a90c8285ee81689e933de1083bf23793
Larissa Carolina <laholiveira001@gmail.com>
2025-03-09 17:48:00 -0300

    L4

---
+++ ANOTAÇOES.php
@@ -0,0 +1,24 @@
+
+<?php
+
+ declare (strict_types=1);// toda variavel tem que ter uma função ,tem que definir o retorno da função (não é obrigatorio ter retorno )
+
+
+
+$numeros[] = floatval($_POST['num' . $i]);//conversão para float
+
+// repetição para criar os dois campos de entrada, se modificar o 2 para qualquer outro numero, indicara a qtd de repetição, e i=1 significa indice partindo do 1 e nao 0
+for ($i = 1; $i <= 2; $i++);
+
+
+ //solucao para usar estrutura de repeticao ..vetor 
+ $numero = $_POST['n_mes']; 
+  $mes= ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+  $nome_mes= "";      
+
+
+
+
+//php -S localhost:8000
+
+?>
--- aula0603.php
+++ aula0603.php
@@ -1,27 +1,53 @@
 <?php
-declare (strict_types=1);// toda variavel tem que ter uma função ,tem que definir o retorno da função (não é obrigatorio ter retorno )
+
+    $idade = 20;
+    if ($idade >= 18)
+        echo "Você é maior de idade!";
+    else {
+        echo "Você é menor de idade!";
+    }
+
+    echo $idade >= 18 ? "Maior de idade!" : "Menor de idade";
+
+    $nota = 6;
+    if ($nota > 6)
+        echo "Acima da média!";
+    elseif ($nota == 6)
+        echo "Na média!";
+    else 
+        echo "Abaixo da média!";
+
+    $mes = 1;
+    switch ($mes){
+        case 1:
+            echo "Janeiro";
+            break;
+        case 2:
+            echo "Fevereiro";
+            break;
+        default:
+            echo "Nenhuma das opções!";
+    }
+
+    $i = 1;
+    while($i<=10){
+        echo "$i<br/>";
+        $valor = 10 + $i++;
+    }
+
+    $i = 1;
+    do {
+        echo "$i<br/>";
+        $i++;
+    } while ($i<=10);
+
+    for ($i=0; $i<10; $i++){
+        echo "$i<br/>";
+    }
+    
 
 
-// se precisar converter float $numeros[] = floatval($_POST['num' . $i]);//conversão para float
-
-// repetição para criar os dois campos de entrada, se modificar o 2 para qualquer outro numero, indicara a qtd de repetição, e i=1 significa indice partindo do 1 e nao 0
-for ($i = 1; $i <= 2; $i++): 
-
-
-$numero = $_POST['n_mes']; 
-                    //solucao para usar estrutura de repeticao ..vetor 
-                    $mes= ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-                    $nome_mes= "";      
-
-?>
-
-<?php
-      function manipularString(string$palavra):void{ //ver slaids
-        echo "<p> a palavra possui ". strlen($palavra)."caracteres </p>";
-        echo "<p> Letra A substituida por 4:",str_replace("a","4",$palavra)."</p>";
-      }  
-
-      function gerarValorAleatorio(int $inicial, $intfinal): int{
+    function gerarValorAleatorio(int $inicial, $intfinal): int{
         return rand($inicial,$final);
       }
       
@@ -33,10 +59,10 @@ $numero = $_POST['n_mes'];
                 echo "<p> O valor gerado foi $valor<\p>";
                 $numero = 3.555555;
                 echo "<p> Mostrando as casas decimais:".number_format($numero,2,",",".")."</p>";
-               
-            } catch (Exception $e) {
-                echo "Erro: " . $e->getMessage();
             }
+        } catch (Exception $e) {
+            echo "Erro: " . $e->getMessage();
         }
+        
+
 ?>
- 
