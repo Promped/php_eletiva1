9b89592e3c4367a9e8bb5260efd9fdec294b5e32
Larissa Carolina <laholiveira001@gmail.com>
2025-04-03 21:54:45 -0300

    03/06 aula

---
+++ ProjetoP2/consultar_produto.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("cabecalho.php");
+    ?>
+
+        <h2>Consultar Produto </h2>
+
+        <div clas="mb-2">
+            <p>Nome do produto: <strong> Produto 1 </strong></p>
+
+        <div clas="mb-2">
+        <p>Descrição do produto: <strong> Produto 1 </strong></p>
+
+        <div clas="mb-2">
+        <p>Valor do produto: <strong> Produto 1 </strong></p>
+
+
+        <div clas="mb-2">
+            <button type="submit" class="btn btn-danger">Excluir</button>
+            <button type="submit" class="btn btn-secundary"
+                onclick="history.back()">Voltar</button>
+        </div>
